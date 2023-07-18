e5d81112733615f2e5ccc001f8aa01c3c8b6de33
analystbruh <chris.martin.data@gmail.com>
2023-07-18 00:37:46 -0400

    updated programs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  // { path: 'programs', component: ProgramsComponent },
+  { path: 'programs', component: ProgramsComponent },
   // { path: 'contact', component: ContactComponent },
   { path: '**', component: HomeComponent },
 ];
